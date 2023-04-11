9e84a897a97065b2e40370edf473b9bb3ccb4660
saurabhvarma <saurabhvarma009@gmail.com>
2023-04-11 19:05:16 +0530

    msg

---
+++ Saurabh.java
@@ -0,0 +1,21 @@
+class Saurabh
+{
+public static void main(String [] args)
+{
+System.out.println("Hello Saurabh");
+}}
+
+
+//git init
+// git status
+//git add Saurabh.java
+// git status
+//git remote add origin https://github.com/saurabhvarm/Firstprogram.git
+// git commit -m "msg"
+//git config --global user.email "saurabhvarma009@gmail.com"
+//git config --global user.name "saurabhvarm"
+//git commit -m msg
+//git push kiran master
+//-----------------------------------
+// git init
+// git pull
